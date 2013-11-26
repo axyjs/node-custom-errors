5fdae1ee5056de7cf9eb0061cb853b50e98d66be
Oleg Grigoriev <go.vasac@gmail.com>
2013-11-26 17:16:41 +0400

    create(): Create a custom error class

--- src/customErrors.js
+++ src/customErrors.js
@@ -8,12 +8,46 @@
 
 "use strict";
 
+var AbstractError;
+
 /**
  * Create a custom error class
  *
+ * @param {String} name
+ *        the name of exception
+ * @param {Function} [parent]
+ *        the parent exception class (constructor) (Error by default)
+ * @param {String} [defmessage]
+ *        the default message
+ * @param {Boolean} [abstract]
+ *        the flag of an abstract class
  * @return {Function}
+ *         the constructor of custom error
  */
-function create() {
+function create(name, parent, defmessage, abstract) {
+    parent = parent || global.Error;
+    defmessage = defmessage || "";
+    function CustomError(message) {
+        var stack;
+        if (abstract) {
+            throw new AbstractError("Error " + name +" is abstract");
+        }
+        this.message = (message === undefined) ? defmessage : message;
+        stack = (new Error()).stack.split("\n");
+        this.stack = name + (this.message ? (": " + this.message) : "");
+        this.stack += "\n" + stack.slice(2).join("\n");
+    }
+    CustomError.prototype = Object.create(parent.prototype);
+    CustomError.prototype.constructor = CustomError;
+    CustomError.prototype.name = name;
+    CustomError.name = name;
+    CustomError.inherit = function inherit(name, defmessage, abstract) {
+        return create(name, CustomError, defmessage, abstract);
+    };
+    return CustomError;
 }
 
+AbstractError = create("AbstractError", Error, "This error class is abstract");
+
 module.exports.create = create;
+module.exports.AbstractError = AbstractError;
--- test/customError_test.js
+++ test/customError_test.js
@@ -2,7 +2,63 @@
 
 var customErrors = require("./../src/customErrors.js");
 
-module.exports.testContents = function (test) {
-    test.equals(typeof customErrors.create, "function");
+module.exports.testCreate = function (test) {
+    var CustomE = customErrors.create("CustomE");
+    try {
+        throw new CustomE("message");
+    } catch (e) {
+        test.ok(e instanceof CustomE);
+        test.ok(e instanceof Error);
+        test.equals(e.message, "message");
+        test.equals(e.name, "CustomE");
+    }
+    test.done();
+};
+
+module.exports.testCreateInherit = function (test) {
+    var One = customErrors.create("One", TypeError),
+        Two = customErrors.create("Two", One),
+        Three = Two.inherit("Three"),
+        e = new Three();
+    test.ok(e instanceof Error);
+    test.ok(e instanceof TypeError);
+    test.ok(e instanceof One);
+    test.ok(e instanceof Two);
+    test.ok(e instanceof Three);
+    test.done();
+};
+
+module.exports.testCreateMessage = function (test) {
+    var CustomE = customErrors.create("CustomE", Error, "default message"),
+        e1 = new CustomE(),
+        e2 = new CustomE("message");
+    test.equals(e1.message, "default message");
+    test.equals(e2.message, "message");
+    test.done();
+};
+
+module.exports.testCreateStack = function (test) {
+    var CustomE = customErrors.create("CustomE"),
+        stack = (new Error()).stack.split("\n"),
+        e = new CustomE("msg"),
+        estack = e.stack.split("\n");
+    test.equals(estack[0], "CustomE: msg");
+    test.equals(estack[1].split("(")[0], stack[1].split("(")[0]);
+    test.equals(estack.slice(2, 5).join("\n"), stack.slice(2, 5).join("\n"));
+    test.done();
+};
+
+module.exports.testCreateAbstract = function (test) {
+    var Base = customErrors.create("Base", Error, null, true),
+        Concrete = Base.inherit(Base, "def"),
+        e = new Concrete();
+    test.equals(e.message, "def");
+    test.ok(e instanceof Base);
+    try {
+        e = new Base();
+        test.ok(false, "Base throws AbstractError");
+    } catch (e) {
+        test.ok(e instanceof customErrors.AbstractError);
+    }
     test.done();
 };
